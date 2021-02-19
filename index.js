7a30c3632fe11be41efced3e21ef9ecc7ca0484a
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-02-18 22:33:49 -0500

    env

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 const apiKey = process.env.PLOTLY_KEY;
 const apiUser = process.env.PLOTLY_USER;
+const url = process.env.WAKATIME_URL;
+
 const plotly = require("plotly")(apiUser, apiKey);
 
 function makePlot(data) {
@@ -53,9 +55,7 @@ function prepareData(data) {
 }
 
 async function main() {
-  const res = await fetch(
-    "https://wakatime.com/share/@2df932ff-33cc-42a9-a0a7-023ed4c13bfa/97adae7e-cc36-46e8-94d5-caf195f07dc8.json"
-  );
+  const res = await fetch(url);
   const { data } = await res.json();
   console.log(data);
   makePlot(prepareData(data));
