9ce09d65f22537441d7086598fb8b2382cf7609e
Tanzim Mokammel <20617008+mtanzim@users.noreply.github.com>
2024-11-27 23:20:03 -0500

    fix: do everything on the server (#12)

--- synonyms.js
+++
@@ -1,9 +0,0 @@
-const synonyms = new Map([
-  ["JSX", "JavaScript"],
-  ["JSON", "Other"],
-  ["YAML", "Other"],
-  ["Markdown", "Other"],
-  ["Docker", "Other"],
-]);
-
-module.exports = { synonyms };
